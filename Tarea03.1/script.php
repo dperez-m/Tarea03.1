f1e159bd94bb81d7a90e42e4e2947418cee0e542
Dani <perdan90@gmail.com>
2023-11-05 18:49:11 +0100

    CONST añadida

--- Tarea03.1/script.php
+++ Tarea03.1/script.php
@@ -14,16 +14,18 @@
 
     <?php
     require_once "utils.php";
+
+    const IMG_SAVE_FOLDER = "data";
     
 if (isset($_FILES["image"])){
     displayImgInfo("image");
     foreach ($_FILES["image"]["error"] as $key => $error){
         $name = basename($_FILES["image"]["name"][$key]);
         $tmp_name = $_FILES["image"]["tmp_name"][$key];
-        if ($error == UPLOAD_ERR_OK && $_FILES["image"]["type"][$key] == "image/jpeg" && move_uploaded_file($tmp_name, "data/$name")) {
+        if ($error == UPLOAD_ERR_OK && $_FILES["image"]["type"][$key] == "image/jpeg" && move_uploaded_file($tmp_name, IMG_SAVE_FOLDER . "/$name")) {
                 echo "</br>Se ha guardado con éxito el fichero $name";
         } else{
-            echo "</br>Se ha producido un error y no se ha podido guardar la imagen $name. </br>Error: " . $_FILES["image"]["error"][$key] . ". Tamaño archivo: " . $_FILES["image"]["size"][$key] / 1000 . "KB";
+            echo "</br>Se ha producido un error y no se ha podido guardar la imagen $name. </br>Error: " . $_FILES["image"]["error"][$key] . ". Tamaño archivo: " . $_FILES["image"]["size"][$key] / 1000 . "KB</br>";
         }
     }
 }
