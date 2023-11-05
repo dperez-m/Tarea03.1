383aad723fe787fcd6252d0fef5ecd9e0ab80be3
Dani <perdan90@gmail.com>
2023-11-05 17:31:31 +0100

    Añadidas nuevas funcionalidades

--- Tarea03.1/script.php
+++ Tarea03.1/script.php
@@ -8,16 +8,26 @@
 <body>
     <h1>Tarea 03.1 de Contorno Servidor</h1>
     <form method="post" id="images" enctype="multipart/form-data">
-    <div><label for="imgs">Adjunte una o varias imágenes, sólo de tipo JPG<input type="file" name="image[]" id="imgs" multiple accept="image/*"></label></div>
+    <div><label for="imgs">Adjunte una o varias imágenes, sólo de tipo JPG<input type="file" name="image[]" id="imgs" multiple accept="image/jpeg"></label></div>
     <div><input type="submit" value="Enviar"></div>
     </form>
 
     <?php
     require_once "utils.php";
-
-    if (isset($_FILES)){
-        displayImgInfo($_FILES);
+if (isset($_FILES["image"])){
+    displayImgInfo("image");
+    foreach ($_FILES["image"]["error"] as $key => $error){
+        $name = basename($_FILES["image"]["name"][$key]);
+        if ($error == UPLOAD_ERR_OK && $_FILES["image"]["type"][$key] == "image/jpeg") {
+            $tmp_name = $_FILES["image"]["tmp_name"][$key];
+            if (move_uploaded_file($tmp_name, "data/$name")){
+                echo "</br>Se ha guardado con éxito el fichero $name";
+            }
+        } else{
+            echo "</br>Se ha producido un error y no se ha podido guardar la imagen $name. </br>Error: " . $_FILES["image"]["error"][$key] . ". Tamaño archivo: " . $_FILES["image"]["size"][$key] / 1000 . "KB";
+        }
     }
+}
     ?>
 </body>
 </html>
--- Tarea03.1/utils.php
+++ Tarea03.1/utils.php
@@ -2,18 +2,19 @@
 
 //displayImgInfo($_FILES);
 
-function displayImgInfo (array $img): void{
-    foreach ($img as $input => $infoArr) {
-        $x = 0;
-        echo "Fichero " . $x + 1 . "</br><ol>";
-        foreach ($infoArr as $infoKey => $infoValue) {
-           echo "<li> $infoKey : $infoValue[$x] </li>";
-        }
+function displayImgInfo (string $filesKey): void{
+    $x = 0;
+    echo "Fichero " . $x + 1 . "</br><ol>";
+    foreach ($_FILES[$filesKey] as $infoKey => $infoValue) {
+        echo "<li> $infoKey : $infoValue[$x] </li>";
+    }
+    if (is_array($_FILES[$filesKey][$infoKey])){
         $x++;
         echo "</ol>Fichero " . $x + 1 . "</br><ol>";
-        foreach ($infoArr as $infoKey => $infoValue) {
+        foreach ($_FILES[$filesKey] as $infoKey => $infoValue) {
            echo "<li> $infoKey : $infoValue[$x] </li>";
         }
     }
 }
+
 ?>
