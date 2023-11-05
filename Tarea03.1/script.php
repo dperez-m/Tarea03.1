cac75ca0cd8166d80d19cdaf192078d476d785d0
Dani <perdan90@gmail.com>
2023-11-05 02:34:19 +0100

    Form arreglado

--- Tarea03.1/script.php
+++ Tarea03.1/script.php
@@ -7,16 +7,17 @@
 </head>
 <body>
     <h1>Tarea 03.1 de Contorno Servidor</h1>
-    <form method="post" id="images" enctype="multipart/form-data"></form>
+    <form method="post" id="images" enctype="multipart/form-data">
     <div><label for="imgs">Adjunte una o varias imágenes, sólo de tipo JPG<input type="file" name="image[]" id="imgs" multiple accept="image/*"></label></div>
     <div><input type="submit" value="Enviar"></div>
+    </form>
 
     <?php
-   /*  require_once "utils.php";
+    require_once "utils.php";
 
     if (isset($_FILES)){
         displayImgInfo($_FILES);
-    } */
+    }
     ?>
 </body>
 </html>
